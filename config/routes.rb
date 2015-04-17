a129e992c11bc3cd2cc4d2bd781ed305bbbbcbc4
cfmeyers <cfmeyers@gmail.com>
2015-04-17 16:15:11 -0400

    disply index memories

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
   resources :sessions, only: [:new, :create, :destroy]
+
+  resources :users do
+    resources :memories
+  end
 
   root "static_pages#home"
   match '/signup', to: 'users#new', via: 'get'
