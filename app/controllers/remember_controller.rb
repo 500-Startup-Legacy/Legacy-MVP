add4f72d8af2b8a47935c7bbb20b7a411c69ae93
cfmeyers <cfmeyers@gmail.com>
2015-04-17 18:55:37 -0400

    add remember controller and show view

---
+++ app/controllers/remember_controller.rb
@@ -0,0 +1,8 @@
+class RememberController < ApplicationController
+
+  def show
+    @user = User.find(params[:user_id])
+    # redirect_to root_path unless @user.public
+  end
+
+end
---
+++ spec/controllers/remember_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe RememberController, type: :controller do
+
+end
