65996556557719ef518e4d37cc6bdcd72bc8cb2c
alexwang949 <alex_cellist@yahoo.com>
2015-04-21 16:27:44 -0400

    added alex account in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@ Memory.delete_all
 collin = User.create(first_name:'Collin', last_name: 'Meyers', email:'cfmeyers@gmail.com', password: 'foobar', password_confirmation: 'foobar')
 ed = User.create(first_name:'Ed', last_name: 'Brooks', email:'edbrooks214@gmail.com', password: 'foobar', password_confirmation: 'foobar')
 abraham = User.create(first_name:'Abraham', last_name: 'Lincoln', email:'abe@gmail.com', password: 'foobar', password_confirmation: 'foobar', public:true)
+alex = User.create(first_name:'alex', last_name: 'wang', email:'alexwang949@gmail.com', password: 'foobar', password_confirmation: 'foobar', public:true)
 
 collin.memorialize!(abraham)
 collin.memorialize!(ed)
