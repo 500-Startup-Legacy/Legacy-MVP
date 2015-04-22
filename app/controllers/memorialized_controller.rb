6fece32213d1b3c87e6ae89edecf94c1953d9e71
cfmeyers <cfmeyers@gmail.com>
2015-04-22 19:18:31 -0400

    fixed authentication so signed in users cannot access other users accounts

--- app/controllers/memorialized_controller.rb
+++ app/controllers/memorialized_controller.rb
@@ -1,5 +1,6 @@
 class MemorializedController < ApplicationController
   before_action :signed_in_user
+  before_action :correct_user
 
   def index
     @user = User.find(params[:user_id])
@@ -34,5 +35,10 @@ class MemorializedController < ApplicationController
 
   def signed_in_user
     redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:user_id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:show]
+  before_action :signed_in_user, only: [:show, :update, :edit]
+  before_action :correct_user, only: [:show, :update, :edit]
+
   def new
     @user = User.new
   end
@@ -47,5 +49,10 @@ class UsersController < ApplicationController
 
   def signed_in_user
     redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
