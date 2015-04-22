cfd170f17f8ab3578d2d9f897217a23c95a120be
alexwang949 <alex_cellist@yahoo.com>
2015-04-22 13:02:46 -0400

    first attempt at dynamically positioning circles, failed.

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,6 +3,22 @@ console.log('linked');
 
 window.onload = function() {
 
+	// var counter = 0;
+
+	// $('.circle').each(function(each){
+	// 	var left = $(this).offset().left;
+	// 	var top = $(this).offset().top;
+
+	// 	counter = counter + 50;
+
+	// 	var newLeft = left + counter;
+	// 	var newTop = top + counter;
+
+	// 	$(this).css('top', newTop);
+	// 	$(this).css('left', newLeft);
+
+	// });
+
 	$('.circle').click(function(event) {
 		console.log(event.target.id);
 	});
