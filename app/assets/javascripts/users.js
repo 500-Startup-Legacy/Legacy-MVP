3d5f6a758df098d3a1ce02bb5b71b487e6030a19
alexwang949 <alex_cellist@yahoo.com>
2015-04-23 18:59:40 -0400

    added font-awesome gem to gemfile, updated asset pipeline with font-awesome, placed icons in appropriate buttons-div

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -35,15 +35,19 @@ window.onload = function() {
 
 	function appendContentToVideoArea(name, content) {
 
-		$('.content-video').empty();
+		$('.content-div').empty();
 
 		var $div = $('<div class="content-div"></div>');
 		var $buttonsDiv = $('<div class="buttons-div"></div>');
 		var $name = $('<h1 class="name">' + name + '</h1>');
-
 		var $memory = $('<p id="paragraph">' + content + '</p>');
+
+		var $gpsIcon = $('<i class="fa fa-map-marker fa-2x"></i>')
+
 		$memory = $memory.text().replace(/,/g, "<br>");
 		$memory = $('<p id="paragraph">' + $memory + '</p>');
+
+		$buttonsDiv.append($gpsIcon);
 
 		$div.append($buttonsDiv);
 		$div.append($name);
