b1260d18af0f8b141cc121e48ab1922207929f6f
alexwang949 <alex_cellist@yahoo.com>
2015-04-23 17:46:20 -0400

    formatting content box for memories

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -38,11 +38,14 @@ window.onload = function() {
 		$('.content-video').empty();
 
 		var $div = $('<div class="content-div"></div>');
-		var $name = $('<h1>' + name + '</h1>');
-		var $memory = $('<p>' + content + '</p>');
+		var $buttonsDiv = $('<div class="buttons-div"></div>');
+		var $name = $('<h1 class="name">' + name + '</h1>');
 
+		var $memory = $('<p id="paragraph">' + content + '</p>');
 		$memory = $memory.text().replace(/,/g, "<br>");
+		$memory = $('<p id="paragraph">' + $memory + '</p>');
 
+		$div.append($buttonsDiv);
 		$div.append($name);
 		$div.append($memory);
 
