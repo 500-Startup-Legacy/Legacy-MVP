0db27f27e7022f138471d165705d0b9fb6970600
cfmeyers <cfmeyers@gmail.com>
2015-04-23 21:58:18 -0400

    change media url param to correct name

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -24,12 +24,39 @@ class TwilioController < ApplicationController
   #   "ApiVersion"=>"2010-04-01"
   # }
 
+# params
+# {
+#   "ToCountry"=>"US", 
+#   "MediaContentType0"=>"image/jpeg", 
+#   "ToState"=>"VA", 
+#   "SmsMessageSid"=>"MMfe14300697df1a1b1e0ec603bfde5723", 
+#   "NumMedia"=>"1", 
+#   "ToCity"=>"VIENNA", 
+#   "FromZip"=>"22209", 
+#   "SmsSid"=>"MMfe14300697df1a1b1e0ec603bfde5723", 
+#   "FromState"=>"VA", 
+#   "SmsStatus"=>"received", 
+#   "FromCity"=>"CENTREVILLE", 
+#   "Body"=>"", 
+#   "FromCountry"=>"US", 
+#   "To"=>"+17033497371", 
+#   "ToZip"=>"20186", 
+#   "MessageSid"=>"MMfe14300697df1a1b1e0ec603bfde5723", 
+#   "AccountSid"=>"AC619f789765e27b4af291c61d77952b80", 
+#   "From"=>"+17038287113", 
+#   "MediaUrl0"=>"https://api.twilio.com/2010-04-01/Accounts/AC619f789765e27b4af291c61d77952b80/Messages/MMfe14300697df1a1b1e0ec603bfde5723/Media/MEb8187fd0fb4206a56dfe7ed6f7d82fff", 
+#   "ApiVersion"=>"2010-04-01", 
+#   "controller"=>"twilio", 
+#   "action"=>"process_sms"
+# }
+
+
   def process_sms
     @to_number = params[:To].slice(2..-1)
     @from_number = params[:From].slice(2..-1)
     @user = User.find_by(phone_number: @from_number)
     @body = params[:Body]
-    @image_url = params[:MediaUrl]
+    @image_url = params[:MediaUrl0]
     if @user
       @memorialized_user = @user.get_memorialized_user_by_twilio_number(@to_number)
       if @memorialized_user
@@ -39,17 +66,17 @@ class TwilioController < ApplicationController
         puts "*"*100
       end
     end
-    puts "^^^^^^^^^^^^^^^^^^^"
-    puts "^^^^^^^^^^^^^^^^^^^"
-    puts "^^^^^^^^^^^^^^^^^^^"
-    puts "^^^^^^^^^^^^^^^^^^^"
-    puts "^^^^^^^^^^^^^^^^^^^"
-    puts params
-    puts "^^^^^^^^^^^^^^^^^^^"
-    puts "^^^^^^^^^^^^^^^^^^^"
-    puts "^^^^^^^^^^^^^^^^^^^"
-    puts "^^^^^^^^^^^^^^^^^^^"
-    puts "^^^^^^^^^^^^^^^^^^^"
+    # puts "^^^^^^^^^^^^^^^^^^^"
+    # puts "^^^^^^^^^^^^^^^^^^^"
+    # puts "^^^^^^^^^^^^^^^^^^^"
+    # puts "^^^^^^^^^^^^^^^^^^^"
+    # puts "^^^^^^^^^^^^^^^^^^^"
+    # puts params
+    # puts "^^^^^^^^^^^^^^^^^^^"
+    # puts "^^^^^^^^^^^^^^^^^^^"
+    # puts "^^^^^^^^^^^^^^^^^^^"
+    # puts "^^^^^^^^^^^^^^^^^^^"
+    # puts "^^^^^^^^^^^^^^^^^^^"
 
     # puts "-"*30
     # if @user
