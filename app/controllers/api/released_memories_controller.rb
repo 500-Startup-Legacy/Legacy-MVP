1bb5ce819b0399d41ca31bf2331d4984b847f09f
cfmeyers <cfmeyers@gmail.com>
2015-04-29 21:48:25 -0400

    suspend ReleasedMemoriesController API until bug fix

--- app/controllers/api/released_memories_controller.rb
+++ app/controllers/api/released_memories_controller.rb
@@ -4,11 +4,12 @@ class Api::ReleasedMemoriesController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @released_user = User.find(params[:released_user_id])
+    payload = {}
 
-    payload = @released_user.memories.select { |memory| memory.memorialized_user_id == @user.id } if @user
-    payload = {'Error': 'No user by that id is in the system'} unless @user
-    payload = {'Error': "User #{@released_user.id} has not released their memories"} unless @released_user.public
-    payload = {'Error': "User #{@user.id} is not signed in"} unless current_user?(@user)
+    # payload = @released_user.memories.select { |memory| memory.memorialized_user_id == @user.id } if @user
+    # payload = {'Error': 'No user by that id is in the system'} unless @user
+    # payload = {'Error': "User #{@released_user.id} has not released their memories"} unless @released_user.public
+    # payload = {'Error': "User #{@user.id} is not signed in"} unless current_user?(@user)
 
     render json: payload
 
