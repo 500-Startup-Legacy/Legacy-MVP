3e35176773f2ca8e1b18b263d8e6909c5e1abc1f
cfmeyers <cfmeyers@gmail.com>
2015-05-01 11:12:02 -0400

    fix static page spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "StaticPages", type: :request do
   describe "The homepage" do
     it "has the correct title" do
       visit root_path
-      expect(page).to have_title('Legacy')
+      expect(page).to have_title('myJourney')
     end
   end
 end
