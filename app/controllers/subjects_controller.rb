dc6a0950fb6aa924afe2e0dc442c86b627c75bc4
cfmeyers <cfmeyers@gmail.com>
2015-05-01 18:54:39 -0400

    fix error message bug once and for all for subjects

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -9,31 +9,15 @@ class SubjectsController < ApplicationController
 
   def create
     @user = User.find(params[:user_id])
-    subject = Subject.create!(subject_params)
-    if subject
-      success_messsage = "You've memorialized #{subject.name}!" 
-      success_messsage += "  Text your memories of #{subject.name} to #{subject.twilio_number}" if subject.twilio_number
+    @subject = Subject.new(subject_params)
+    if @subject.save
+      success_messsage = "You've memorialized #{@subject.name}!" 
+      success_messsage += "  Text your memories of #{@subject.name} to #{@subject.twilio_number}" if @subject.twilio_number
       flash[:success] = success_messsage
       redirect_to @user
     else
-        # flash[:fail]  
       render 'new'
     end
-    # @memorialized = User.find_by(email: params[:email])
-    # relationship = @memorializer.memorialize!(@memorialized, params[:group_tag]) if @memorialized
-    # if @memorialized && relationship
-    #   success_messsage = "You've memorialized #{@memorialized.full_name}!" 
-    #   success_messsage += "  Text your memories of #{@memorialized.full_name} to #{relationship.twilio_number}" if relationship.twilio_number
-    #   flash[:success] = success_messsage
-    #   redirect_to @memorializer
-    # else
-    #   if @memorialized
-    #     flash[:fail] = "You're already memorializing #{@memorialized.full_name}!" 
-    #   else
-    #     flash[:fail] = "We couldn't find a user with this email address" 
-    #   end
-    #   render 'new'
-    # end
   end
 
   def subject_params
